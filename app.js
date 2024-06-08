9bc90a6338e62247e8704f65e082f043d5c32697
lc3lx <140909706+lc3lx@users.noreply.github.com>
2024-06-08 19:19:07 +0300

    app.js

--- app.js
+++ app.js
@@ -22,38 +22,10 @@ app.get("/getorder",async(req,res) =>{
 
 app.post('/webhook',async (req, res) => {
     const orderr = new Order({
-        id : req.body.id,
-        cart_reference_id: req.body.cart_reference_id,
-        reference_id: req.body.reference_id,
-        urls:req.body.urls,
-        date:req.body.date,
-        updated_at:req.body.updated_at,
-        source:req.body.source,
-        source_device:req.body.source_device,
-        source_details:req.body.source_details,
-        status:req.body.status,
-        is_price_quote:req.body.is_price_quote,
-        payment_method:req.body.payment_method,
-        receipt_image:req.body.receipt_image,
-        first_complete_at:req.body.first_complete_at,
-        currency:req.body.currency,
-        amounts:req.body.amounts,
-        can_cancel:req.body.can_cancel,
-        show_weight:req.body.show_weight,
-        can_reorder:req.body.can_reorder,
-        is_pending_payment:req.body.is_pending_payment,
-        pending_payment_ends_at:req.body.pending_payment_ends_at,
-        total_weight:req.body.total_weight,
-        rating_link:req.body.rating_link,
-        shipping:req.body.shipping,
-        shipments:req.body.shipments,
-        shipment_branch:req.body.shipment_branch,
-        customer:req.body.customer,
-        items:req.body.items,
-        bank:req.body.bank,
-        product_reservations:req.body.product_reservations,
-        tags:req.body.tags,
-        store:req.body.store
+        event:req.body.event,
+        merchant:req.body.merchant,
+        created_at:req.body.created_at,
+        data: req.body.data
 
     })
     const resulat = await orderr.save()
